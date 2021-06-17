2ae22d7717394da74d5a751727c10aa896ca1b6e
nitin7516 <40827296+nitin7516@users.noreply.github.com>
2021-06-17 11:45:46 +0530

    Update consumer.py

--- kafka_consumer/consumer.py
+++ kafka_consumer/consumer.py
@@ -14,6 +14,39 @@ app = Flask(__name__)
 
 cf_port = os.getenv("PORT")
 
+@app.route("/")
+def start_consumer():
+   load_dotenv(os.path.join(os.path.dirname(__file__), ".env"))
+   opts['brokers'] = os.getenv("KAFKA_BROKERS_SASL")
+   opts['rest_endpoint'] = os.getenv("KAFKA_HTTP_URL")
+   opts['api_key'] = os.getenv("PASSWORD")
+   opts['username'] = os.getenv("USER")
+   opts['topic_name'] = os.getenv("TOPIC")
+
+   print('Kafka Endpoints: {0}'.format(opts['brokers']))
+   print('Admin REST Endpoint: {0}'.format(opts['rest_endpoint']))
+   print('API_KEY: {0}'.format(opts['api_key']))
+
+   if any(k not in opts for k in ('brokers', 'rest_endpoint', 'api_key')):
+   	print('Error - Failed to retrieve options. Check that app is bound to an Event Streams service or that command line options are correct.')
+   	sys.exit(-1)
+
+   # Use Event Streams' REST admin API to create the topic
+   # with 1 partition and a retention period of 24 hours.
+   rest_client = rest.EventStreamsRest(opts['rest_endpoint'], opts['api_key'])
+   print('Creating the topic {0} with Admin REST API'.format(opts['topic_name']))
+   response = rest_client.create_topic(opts['topic_name'], 1, 24)
+   print(response.text)
+
+   # Use Event Streams' REST admin API to list the existing topics
+   print('Admin REST Listing Topics:')
+   response = rest_client.list_topics()
+   print(response.text)
+
+   run_tasks(opts, run_consumer, consumer)
+   return "Consumer Started";
+   
+
 @app.route("/consume")
 def consume_json():
    opts = {}
@@ -47,7 +80,7 @@ def consume_json():
    response = rest_client.list_topics()
    print(response.text)
 
-   run_tasks(opts, run_consumer, consumer)
+   #run_tasks(opts, run_consumer, consumer)
    SITE_ROOT = os.path.realpath(os.path.dirname(__file__))
    json_url = os.path.join(SITE_ROOT, "coordinates.json")
    print("*************11**",os.path)
